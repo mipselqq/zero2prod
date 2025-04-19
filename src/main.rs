3fe107e66bdfa8c4739de28e83be18f1282eba3c
Ilya Semyonov <mipselqq@gmail.com>
2025-04-20 02:56:51 +0300

    feat: hello world server
    A test commit body

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+extern crate core;
+
+use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+use std::io;
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("0.0.0.0:8000")?
+    .run()
+    .await
 }
