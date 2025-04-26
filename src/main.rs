f4b4bedde4e6679e2cf2fd1c05c0596362383f75
Ilya Semyonov <mipselqq@gmail.com>
2025-04-27 00:13:05 +0300

    feat!: add basic /health_check endpoint and get rid of example handlers

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,14 @@
-extern crate core;
-
-use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 use std::io;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
 
 #[actix_web::main]
 async fn main() -> Result<(), io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("0.0.0.0:8000")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("0.0.0.0:8000")?
+        .run()
+        .await
 }
