41f931240a8e723c93b119e17589b9ea3f6a3757
Ilya Semyonov <mipselqq@gmail.com>
2025-04-27 01:18:59 +0300

    chore: factor out application start logic into lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 use std::io;
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> Result<(), io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("0.0.0.0:8000")?
-        .run()
-        .await
+    run().await
 }
