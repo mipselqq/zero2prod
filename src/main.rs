1516427ce365afdb85bc21984ecead6048d2ef1f
Ilya Semyonov <mipselqq@gmail.com>
2025-04-27 01:48:52 +0300

    test(health_check): implement integration test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::io;
-use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> Result<(), io::Error> {
-    run().await
+    zero2prod::run()?.await
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,21 @@
+#[actix_web::test]
+async fn health_check_works() {
+    spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Test request should execute");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = zero2prod::run().expect("Adress should be bound correctly");
+
+    let _ = actix_rt::spawn(server);
+}
