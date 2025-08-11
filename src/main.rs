395b062da551dfb12a5cfbe55f4ba09c2750dc22
Ilya Semyonov <mipselqq@gmail.com>
2025-08-11 22:13:38 +0300

    test: run health_check and future tests on random port refactor: listen app on specified listener instead of hardcoded adress

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::net::TcpListener;
+
 use zero2prod::run_app;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run_app()?.await
+    run_app(TcpListener::bind("0.0.0.0:8000").expect("OS should bind listener"))?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,18 +1,26 @@
+use std::net::TcpListener;
+
 use zero2prod::run_app;
 
 #[tokio::test]
 async fn health_check_responds_ok() {
-    spawn_app();
+    let addr = spawn_app();
 
-    let response = reqwest::get("http://localhost:8000/health_check")
+    let response = reqwest::get(format!("{addr}/health_check"))
         .await
         .expect("Request should proceed");
-
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = run_app().expect("App should run");
+fn spawn_app() -> String {
+    let listener =
+        TcpListener::bind("0.0.0.0:0").expect("OS should bind app listener to random port");
+
+    let port = listener.local_addr().unwrap().port();
+    let server = run_app(listener).expect("App should run");
+
     tokio::spawn(server);
+
+    format!("http://localhost:{port}")
 }
