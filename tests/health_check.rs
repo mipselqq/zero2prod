55eaa5107506b68dde443d3808ae022aa044e6d8
Ilya Semyonov <mipselqq@gmail.com>
2025-08-18 21:55:29 +0300

    fmt: run cargo fmt

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,16 +1,63 @@
 use std::net::TcpListener;
 
+use actix_web::dev::Response;
+use reqwest::{Client, StatusCode};
 use zero2prod::run_app;
 
 #[tokio::test]
-async fn health_check_responds_ok() {
+async fn health_check_returns_success() {
     let addr = spawn_app();
 
     let response = reqwest::get(format!("{addr}/health_check"))
         .await
-        .expect("Request should proceed");
+        .expect("Request should execute");
+
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_ok_for_valid_form_data() {
+    let addr = spawn_app();
+    let client = Client::new();
+
+    let body = "name=le%20guin&enail=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(format!("{addr}/subscriptions"))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Request should execute");
+
+    assert_eq!(StatusCode::OK, response.status());
+}
+
+#[tokio::test]
+async fn subscibe_returns_bad_request_for_missing_data() {
+    let addr = spawn_app();
+    let client = Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(format!("{addr}/subscriptions"))
+            .header("Content-Type", "application/x-www-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Request should execute");
+
+        assert_eq!(
+            StatusCode::BAD_REQUEST,
+            response.status(),
+            "App didn't fail with BAD_REQUEST when payload was {error_message}"
+        )
+    }
 }
 
 fn spawn_app() -> String {
