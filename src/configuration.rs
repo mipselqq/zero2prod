029323a3e276d7d8e394c6832487d2ae2fa3a129
Ilya Semyonov <mipselqq@gmail.com>
2025-09-22 11:25:42 +0300

    feat: read port from config in bin

---
+++ src/configuration.rs
@@ -0,0 +1,43 @@
+use std::fmt::format;
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+
+#[derive(Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn read_configuration() -> Result<Settings, config::ConfigError> {
+    let settings = config::Config::builder()
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
+        .build()?;
+
+    settings.try_deserialize::<Settings>()
+}
+
+impl DatabaseSettings {
+    pub fn format_connection_string(&self) -> String {
+        let Self {
+            username,
+            password,
+            host,
+            port,
+            database_name,
+        } = self;
+
+        format!("postgres://{username}:{password}@{host}:{port}/{database_name}")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use std::net::TcpListener;
+mod configuration;
 
+use std::net::TcpListener;
 use zero2prod::run_app;
+
+use crate::configuration::read_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let configuration = read_configuration().expect("Configuration should be red");
+    let address = format!("0.0.0.0:{}", configuration.application_port);
+
     run_app(TcpListener::bind("0.0.0.0:8000").expect("OS should bind listener"))?.await
 }
