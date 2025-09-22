0f604eb126eade589f137c259fba2c004cfa0e45
Ilya Semyonov <mipselqq@gmail.com>
2025-09-22 22:44:30 +0300

    chore(Cargo.toml): add tracing-subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 mod configuration;
 
 use crate::configuration::read_configuration;
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::run_app;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-
     let configuration = read_configuration().expect("Configuration should be red");
     let connection = PgPool::connect(&configuration.database.format_connection_string())
         .await
