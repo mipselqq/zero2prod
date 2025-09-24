3fcfc2e7a9bffee5fb01736fa1de63af829c5329
Ilya Semyonov <mipselqq@gmail.com>
2025-09-24 12:39:56 +0300

    refactor(telemetry) factor out to new module

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod configuration;
 pub mod routes;
 pub mod startup;
+pub mod telemetry;
 
 pub use configuration::*;
 pub use routes::*;
 pub use startup::*;
+pub use telemetry::*;
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,8 @@ mod configuration;
 use crate::configuration::read_configuration;
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::{Subscriber, subscriber::set_global_default};
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 use zero2prod::run_app;
+use zero2prod::telemetry::{build_subscriber, setup_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -27,20 +24,4 @@ async fn main() -> Result<(), std::io::Error> {
         connection,
     )?
     .await
-}
-
-pub fn build_subscriber(name: &str, env_filter: &str) -> impl Subscriber + Send + Sync {
-    let env_filter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer = BunyanFormattingLayer::new(name.into(), std::io::stdout);
-
-    Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer)
-}
-
-pub fn setup_subscriber(subscriber: impl Subscriber + Send + Sync) {
-    LogTracer::init().expect("LogTracer should init");
-    set_global_default(subscriber).expect("Default subscriber should set");
 }
