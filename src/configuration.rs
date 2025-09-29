60c30debe555886427387586877aefebf762dd8a
Ilya Semyonov <mipselqq@gmail.com>
2025-09-29 21:01:44 +0300

    feat: use conditional config depending on APP_ENVIRONMENT

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,3 +1,5 @@
+use std::{env, fmt::format};
+
 use secrecy::{ExposeSecret, SecretString};
 use serde::Deserialize;
 
@@ -23,14 +25,52 @@ pub struct ApplicationSettings {
 }
 
 pub fn read_configuration() -> Result<Settings, config::ConfigError> {
+    let base_path = env::current_dir().expect("Current directory should be gotten");
+    let configuration_directory = base_path.join("configuration");
+
+    let environment: Environment = env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Invalid or absent APP_ENVIRONMENT");
+    let environment_filename = format!("{}.yaml", environment.as_str());
+
     let settings = config::Config::builder()
-        .add_source(config::File::new(
-            "configuration.yaml",
-            config::FileFormat::Yaml,
+        .add_source(config::File::from(configuration_directory.join("base")))
+        .add_source(config::File::from(
+            configuration_directory.join(environment_filename),
         ))
         .build()?;
 
     settings.try_deserialize::<Settings>()
+}
+
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &str {
+        match self {
+            Self::Local => "local",
+            Self::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            unknown => Err(format!(
+                "{unknown} is not a supported environment. \"
+                Use either `local` or `production`."
+            )),
+        }
+    }
 }
 
 impl DatabaseSettings {
